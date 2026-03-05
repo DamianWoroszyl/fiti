e1f588708db0edfca28608101da7806446156e2c
Damian Woroszyl <damian.woroszyl.dev@gmail.com>
2026-03-05 16:03:15 +0000

    Fix navigation

--- app/src/main/java/com/fullrandom/fiti/ui/di/UiModule.kt
+++ app/src/main/java/com/fullrandom/fiti/ui/di/UiModule.kt
@@ -6,11 +6,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object UiModule {
 
+    @Singleton
     @Provides
     fun provideNavigator(navigatorImpl: NavigatorImpl): Navigator = navigatorImpl
 
